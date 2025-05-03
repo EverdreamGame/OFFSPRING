0694435a0008d55bffd06fb7a7b00641e766e16b
Lluc <lluc.estruch@gmail.com>
2025-05-03 20:45:30 +0200

    coleccionables particulas

--- OFFSPRING/Assets/Scripts/MemoryVaults/TriggerMemoryVaultByCollision.cs
+++ OFFSPRING/Assets/Scripts/MemoryVaults/TriggerMemoryVaultByCollision.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -5,8 +6,17 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Collider))]
 public class TriggerMemoryVaultByCollision : MonoBehaviour
 {
-    [Header("*Arrastrar prefab de CollectableParticleSystem como hijo*")]
-    [Space]
+    [HideInInspector][SerializeField] public GameObject collectedEffectPrefab;
+
+    private float shrinkAnimationDuration = 1f;
+
+    private static readonly AnimationCurve shrinkCurve = new AnimationCurve(
+        new Keyframe(0f, 1f, 0f, 4f),
+        new Keyframe(0.3f, 1.2f),
+        new Keyframe(1f, 0f, -4f, 0f)
+    );
+
+
 
     [Tooltip("Eventos adicionales que se ejecutan al recoger el objeto")]
     public UnityEvent onTriggerEnter;
@@ -15,14 +25,44 @@ public class TriggerMemoryVaultByCollision : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            //GetComponentInChildren<ParticleSystem>().Stop();
-            GetComponentInChildren<ParticleSystem>().Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-            GetComponentInChildren<ParticleSystem>().loop = false;
-            transform.GetChild(0).GetComponentInChildren<ParticleSystem>().Play();
-
-            Destroy(gameObject, 1f);
+            StartCoroutine(ShrinkCoroutine(transform.localScale));
 
             onTriggerEnter.Invoke();
         }
+    }
+
+    IEnumerator ShrinkCoroutine(Vector3 originalScale)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < shrinkAnimationDuration)
+        {
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / shrinkAnimationDuration);
+
+            // Usa la curva para calcular el factor de escala
+            float scaleFactor = shrinkCurve.Evaluate(t);
+
+            transform.localScale = originalScale * scaleFactor;
+
+            yield return null;
+        }
+
+        transform.localScale = Vector3.zero;
+
+        ParticleBurstAnimation();
+
+        Destroy(gameObject);
+    }
+
+
+    // *** Funcionará solo si la jerarquia del prefab del sistema de partículas es correcto!!
+    private void ParticleBurstAnimation()
+    {
+        ParticleSystem auraPS = GetComponentInChildren<ParticleSystem>();
+
+        auraPS.Stop();
+
+        Instantiate(collectedEffectPrefab, auraPS.transform.position, auraPS.transform.rotation);
     }
 }
