da95fbe92d0d422d0ca2c55d9b442a070b301e6f
Lluc <lluc.estruch@gmail.com>
2025-05-18 18:58:55 +0200

    este baul esta ocupado por un padre y su hijo

--- OFFSPRING/Assets/Scripts/MemoryVault/MemorySlot.cs
+++ OFFSPRING/Assets/Scripts/MemoryVault/MemorySlot.cs
@@ -7,16 +7,19 @@ using System.Collections;
 public class MemorySlot : MonoBehaviour, IPointerEnterHandler
 {
     [SerializeField] SO_Collectable memory;
+    public SO_Collectable memoryUnknown;
 
     private TMP_Text memoryNameText;
     private TMP_Text memoryDescriptionText;
 
     private ShowMemoryDetails showMemoryDetails;
 
+    private bool unlocked;
+
     private void Start()
     {
         memoryNameText = GameObject.Find("MemoryName").GetComponent<TMP_Text>();
-        memoryNameText = GameObject.Find("MemoryDescription").GetComponent<TMP_Text>();
+        memoryDescriptionText = GameObject.Find("MemoryDescription").GetComponent<TMP_Text>();
 
         showMemoryDetails = GameObject.FindAnyObjectByType<ShowMemoryDetails>().GetComponent<ShowMemoryDetails>();
     }
@@ -25,13 +28,29 @@ public class MemorySlot : MonoBehaviour, IPointerEnterHandler
     {
         if (memory == null) return;
 
-        //gameObject.GetComponentInChildren<Image>(true).sprite = memory.render_2D; // No funcionaba pillaba el mismo gameobject >:(
-        gameObject.transform.GetChild(0).GetComponent<Image>().sprite = memory.render_2D;
+        unlocked = MemoriesCollectedManager.collectables.Contains(memory);
+
+        if (!unlocked)
+        {
+            gameObject.transform.GetChild(0).GetComponent<Image>().sprite = memoryUnknown.render_2D;
+        }
+        else
+        {
+            gameObject.transform.GetChild(0).GetComponent<Image>().sprite = memory.render_2D;
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        showMemoryDetails.memorySelected = memory;
+        if (!unlocked)
+        {
+            showMemoryDetails.memorySelected = memoryUnknown;
+        }
+        else
+        {
+            showMemoryDetails.memorySelected = memory;
+        }
+        
         showMemoryDetails.SetMemoryDetails();
     }
 }
